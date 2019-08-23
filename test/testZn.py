e8a492e4d8cce0b5d44208a524d8fca5e79ac78d
KiteWindZN <15605189713@163.com>
2019-08-23 08:00:26 +0800

    update

--- test/testZn.py
+++ test/testZn.py
@@ -55,7 +55,7 @@ def schedule_mst(stations,vehicles,station_list1,station_list2,station_list3,mst
             createEntity.cal_station_area_weight(choose_station)
             if max_height == 5.6:
                 print ("aaaa")
-            while max_height < choose_vehicle.length*0.85:
+            while max_height < choose_vehicle.length*0.9:
 
                 nobor_list = mst[s_id]
                 tmp_dis = sys.maxsize
@@ -84,7 +84,7 @@ def schedule_mst(stations,vehicles,station_list1,station_list2,station_list3,mst
             if next_s_id == "-1" or dis_cost > choose_vehicle.startPrice:
                 continue
 
-            while max_height < choose_vehicle.length*0.85:
+            while max_height < choose_vehicle.length*0.9:
 
                 nobor_list = mst[s_id]
                 tmp_dis = sys.maxsize
@@ -160,7 +160,7 @@ def schedule_mst(stations,vehicles,station_list1,station_list2,station_list3,mst
             max_height = skyLine.skyline(choose_vehicle, choose_station)
             createEntity.cal_station_area_weight(choose_station)
 
-            while max_height < choose_vehicle.length*0.9:
+            while max_height < choose_vehicle.length*0.85:
 
                 nobor_list = mst[s_id]
                 tmp_dis = sys.maxsize
@@ -189,7 +189,7 @@ def schedule_mst(stations,vehicles,station_list1,station_list2,station_list3,mst
             if next_s_id == "-1" or dis_cost > choose_vehicle.startPrice:
                 continue
 
-            while max_height < choose_vehicle.length*0.9:
+            while max_height < choose_vehicle.length*0.85:
 
                 nobor_list = mst[s_id]
                 tmp_dis = sys.maxsize
@@ -453,6 +453,90 @@ def avg_height(lines):
     return avg_h
 
 
+def pick_bins(vehicle_list,stations,map,time):
+    for v in vehicle_list:
+        if v.max_height < v.length * 0.4:
+            for v1 in vehicle_list:
+                if v1.id == v.id:
+                    continue
+                if v1.max_height > v1.length *0.9:
+                    continue
+                s_id = v1.path[-1]
+                if v1.usedTime + time[s_id][v.path[0]] +stations[v.path[0]].loading_time <= 600 and map[s_id][v.path[0]] * v1.perPrice < v.startPrice and v1.length <= stations[v.path[0]].vehicle_limit:
+                    station = stations[s_id]
+                    tmp_vehicle = entity.Vehicle(v1.id, v1.length, v1.width, v1.weight, v1.startPrice, v1.perPrice)
+                    tmp_station=entity.Station(s_id,station.vehicle_limit,station.loading_time)
+                    for b in v.bin_list:
+                        tmp_station.station_bin[b.local_station]=[]
+                    for b in v.bin_list:
+                        tmp_bin=entity.Bin(b.id,b.length,b.width,b.weight,b.local_station)
+                        tmp_station.binList.append(tmp_bin)
+                        tmp_station.station_bin[b.local_station].append(tmp_bin)
+                        tmp_station.isEmpty=False
+                    for b in v1.bin_list:
+                        tmp_bin=entity.Bin(b.id,b.length,b.width,b.weight,b.local_station)
+                        tmp_bin.pointList.append(b.pointList[0])
+                        tmp_bin.pointList.append(b.pointList[1])
+                        tmp_bin.pointList.append(b.pointList[2])
+                        tmp_bin.pointList.append(b.pointList[3])
+                        tmp_vehicle.bin_list.append(tmp_bin)
+                    for l in v1.lines:
+                        tmp_line = entity.Line(entity.Point(l.start.x,l.start.y),entity.Point(l.end.x, l.end.y),l.left_height,l.right_height)
+                        tmp_vehicle.lines.append(tmp_line)
+                    skyLine.skyline(tmp_vehicle,tmp_station)
+                    if tmp_station.isEmpty ==True:
+                        vehicle_list.remove(v)
+                        vehicle_list.remove(v1)
+                        tmp_vehicle.usedTime = v1.usedTime + time[s_id][v.path[0]] +stations[v.path[0]].loading_time
+                        vehicle_list.append(tmp_vehicle)
+                        print("+++++OK")
+                        break
+
+def change_vehicle(vehicle_list,vehicles):
+    for i in range(len(vehicle_list)):
+        v=vehicle_list[i]
+        if v.length == 17.5 and v.max_height<= 9.6:
+            for v2 in vehicles:
+                v1=geneticAlgm.create_new_vehicle(v2)
+                if v1.is_available == True:
+                    tmp_station = entity.Station(v.path[0], v1.length, 0)
+                    for b in v.bin_list:
+                        tmp_bin=entity.Bin(b.id,b.length,b.width,b.weight,b.local_station)
+                        tmp_station.binList.append(tmp_bin)
+                        tmp_station.isEmpty=False
+                    skyLine.skyline(v1,tmp_station)
+                    if tmp_station.isEmpty == True:
+                        v1.path=[]
+                        for p in v.path:
+                            v1.path.append(p)
+                        vehicle_list.remove(v)
+                        vehicle_list.append(v1)
+                        v2.is_available = False
+                        i = i-1
+                        print("+++++++++")
+                        break
+        elif v.length == 9.6 and v.max_height<= 7.8:
+            for v2 in vehicles:
+                v1=geneticAlgm.create_new_vehicle(v2)
+                if v1.is_available == True:
+                    tmp_station = entity.Station(v.path[0], v1.length, 0)
+                    for b in v.bin_list:
+                        tmp_bin=entity.Bin(b.id,b.length,b.width,b.weight,b.local_station)
+                        tmp_station.binList.append(tmp_bin)
+                        tmp_station.isEmpty=False
+                    skyLine.skyline(v1,tmp_station)
+                    if tmp_station.isEmpty == True:
+
+                        v1.path = []
+                        for p in v.path:
+                            v1.path.append(p)
+                        vehicle_list.remove(v)
+                        vehicle_list.append(v1)
+                        v2.is_available = False
+                        i=i-1
+                        print("--------")
+                        break
+
 def random_list(list):
     res_list=[]
     while len(list)>0:
@@ -518,6 +602,9 @@ def myTest():
     #vehicle_list = schedule(stations,vehicles,map,time)
     #geneticAlgm.check_vehicle_list(vehicle_list)
     vehicle_list1=schedule_mst(stations,vehicles,station_list3,station_list1,station_list2,map,time)
+    pick_bins(vehicle_list1,stations,map,time)
+    change_vehicle(vehicle_list1, vehicles)
+    geneticAlgm.check_vehicle_list(vehicle_list1)
     #gene,cost,rate = genetic(vehicles,stations,map,time,200)
     #vehicle_list = geneticAlgm.schedule_gene(gene, vehicles, stations, map, time)
     #total_cost, total_rate = geneticAlgm.cal_final_result(vehicle_list, map)
